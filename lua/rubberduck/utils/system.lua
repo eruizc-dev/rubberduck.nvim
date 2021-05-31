b25c75a3b66ad86d6729cd825a3dcde0116505f1
Emiliano Ruiz Carletti <contact@eruizc.dev>
2021-05-30 21:36:02 -0300

    Update system utils

--- lua/rubberduck/utils/system.lua
+++ lua/rubberduck/utils/system.lua
@@ -10,4 +10,19 @@ function system.get_system_name()
   return "unknown"
 end
 
+function system.get_open_command()
+  local sys_name = system.get_system_name()
+
+  if sys_name == "Linux" then return "xdg-open" end
+  if sys_name == "Darwin" then return "open" end
+  error("Unsupported system '" .. sys_name .. "'")
+end
+
+function system.open(link)
+  if type(link) ~= "string" then error("Expected link to be type 'string' but got '" .. type(link) .. "'") end
+  local open_command = system.get_open_command()
+  local vim_command = string.format("silent !%s '%s'", open_command, link)
+  vim.api.nvim_command(vim_command)
+end
+
 return system
--- lua/spec/rubberduck/utils/system_spec.lua
+++ lua/spec/rubberduck/utils/system_spec.lua
@@ -1,12 +1,12 @@
 local mock = require("luassert.mock")
+local stub = require("luassert.stub")
 local random = require("rubberduck.utils.random")
 
-local system = require("rubberduck.utils.system")
-
 describe("system", function()
-  local Job = mock(require("plenary.job"), true)
-
+  local system = require("rubberduck.utils.system")
   describe("get_name", function()
+    local Job = mock(require("plenary.job"), true)
+
     it("returns Linux if system is Linux", function()
       Job.new.returns({ sync = function() return { "Linux" } end })
       local result = system.get_system_name()
@@ -25,6 +25,56 @@ describe("system", function()
         Job.new.returns({ sync = function() return { sys_name } end })
         local result = system.get_system_name()
         assert.equal(sys_name, result)
+      end
+    end)
+  end)
+
+  describe("get_open_command", function()
+    after_each(function()
+      if system.get_system_name.revert then
+        system.get_system_name:revert()
+      end
+    end)
+
+    it("excepts when system is unknown", function()
+      stub(system, "get_system_name", function() return "unknown" end)
+      assert.has_error(function() system.get_open_command() end, "Unsupported system 'unknown'")
+    end)
+
+    it("excepts when system is not supported", function()
+      local sys_name = random.string(8)
+      stub(system, "get_system_name", function() return sys_name end)
+      assert.has_error(function() system.get_open_command() end, "Unsupported system '" .. sys_name .. "'")
+    end)
+
+    it("returns xdg-open for system Linux", function()
+      stub(system, "get_system_name", function() return "Linux" end)
+      local cmd = system.get_open_command()
+      assert.equal("xdg-open", cmd)
+    end)
+
+    it("returns open for system Darwin", function()
+      stub(system, "get_system_name", function() return "Darwin" end)
+      local cmd = system.get_open_command()
+      assert.equal("open", cmd)
+    end)
+  end)
+
+  describe("open", function()
+    it("excepts if argument is not a string", function()
+      local invalid_args = { 13, function() end, { results = 10 }, { 1, 2, 3 } }
+      for _, arg in ipairs(invalid_args) do
+        assert.has_error(function() system.open(arg) end, "Expected link to be type 'string' but got '" .. type(arg) .. "'")
+      end
+    end)
+
+    it("executes correct command", function()
+      stub(vim.api, "nvim_command")
+      stub(system, "get_open_command", function() return "open" end)
+      for _ = 1, 128 do
+        local link = random.string()
+        system.open(link)
+        assert.stub(vim.api.nvim_command).was_called_with("silent !open '"..link.."'")
       end
     end)
   end)
